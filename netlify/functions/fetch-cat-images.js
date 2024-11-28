ef96c6d637b174e5078acf180b62f6eb2fbf8d9b
henia87 <heni.abranyi@gmail.com>
2024-11-28 21:54:22 +0100

    Fix breeds list spinner

--- netlify/functions/fetch-cat-images.js
+++ netlify/functions/fetch-cat-images.js
@@ -2,7 +2,7 @@ exports.handler = async function(event, context) {
     const api_key = process.env.API_KEY;
     
     try {
-        const response = await fetch(`https://api.thecatapi.com/v1/images/search?limit=50&size=full&api_key=${api_key}`);
+        const response = await fetch(`https://api.thecatapi.com/v1/images/search?limit=20&size=full&api_key=${api_key}`);
         const data = await response.json();
     
         return {
